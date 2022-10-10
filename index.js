eddda89915b34550e71290eb16886a95372d9ca8
zan <olsencortuna@gmail.com>
2022-10-10 16:39:19 +0800

    hehe

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-
-
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import accommodatorRoutes from './routes/accommodator.js'
-import roomRoutes from './routes/room.js'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import accommodatorRoutes from "./routes/accommodator.js";
+import roomRoutes from "./routes/room.js";
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 // app.use('/accommodation', accommodationRoutes);
-app.use('/accommodator', accommodatorRoutes);
-app.use('/rooms',roomRoutes)
+app.use("/accommodator", accommodatorRoutes);
+app.use("/rooms", roomRoutes);
 
-app.get('/',(res) => {res.send('Hello to Room hunt API');});
+app.get("/", (res) => {
+  res.send("Hello to Room hunt API");
+});
 
-const PORT = process.env.PORT|| 8000;
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
-
