33eac3f06980036af0ad831bf327899bb8ac5ab4
zan <olsencortuna@gmail.com>
2022-10-10 17:11:40 +0800

    env

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import dotenv from "dotenv";
 import accommodatorRoutes from "./routes/accommodator.js";
 import roomRoutes from "./routes/room.js";
 const app = express();
-dotenv.config({path: 'ENV_FILENAME'});
-// dotenv.config();
+// dotenv.config({ path: "ENV_FILENAME" });
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -24,10 +24,10 @@ const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useMongoClient: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on http://localhost:${PORT}`)
