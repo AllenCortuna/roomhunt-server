6fe4115b21c795b94b190a2b42b92fe68fd4faa2
zan <olsencortuna@gmail.com>
2022-10-11 16:18:50 +0800

    stable email verification with gmail

--- tools/mail.js
+++ tools/mail.js
@@ -1,14 +1,13 @@
+import nodemailer from "nodemailer";
 
-import nodemailer from 'nodemailer'
-
-export const mailTransport = () => nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: process.env.MT_USER,
-    pass: process.env.MT_PASSWORD,
-  },
-});
+// export const mailTransport = () => nodemailer.createTransport({
+//   host: "smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: process.env.MT_USER,
+//     pass: process.env.MT_PASSWORD,
+//   },
+// });
 
 let otp = "";
 export const generateOTP = () => {
@@ -19,3 +18,27 @@ export const generateOTP = () => {
   return otp;
 };
 
+export const mailTransport = () =>
+  nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    auth: {
+      user: "olsencortuna@gmail.com",
+      pass: "yeopsbeprvyefdvt",
+    },
+  });
+
+// var mailOptions = {
+//   from: 'youremail@gmail.com',
+//   to: 'myfriend@yahoo.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+//
+// mailTransport.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
