abe1af3dc261232a0e307058594d709c3bb5fe61
zan <olsencortuna@gmail.com>
2022-11-02 18:59:24 +0800

    fix: review delay

--- controllers/review.js
+++ controllers/review.js
@@ -7,71 +7,81 @@ import Review from "../models/review.js";
 const router = express.Router();
 
 export const reviewRoom = async (req, res) => {
-  const { room, senderId, review } = req.body;
-
-  if (!mongoose.Types.ObjectId.isValid(room))
-    return res.status(404).send(`No room with id: ${room}`);
-
-  const reviewed = await Review.findOne({
-    room: room,
-    senderId: senderId,
-  });
-  // WARN: REVIEWED
-  if (reviewed) {
-    await reviewed.updateOne({ room, senderId, review });
-    await reviewed.save();
-    const total = await Review.countDocuments({
-      room: room,
-    });
-    const roomReview = await Review.find({
-      room: room,
-    })
-    const sum = roomReview.map((a) => a.review);
-    function calculateAverage(arr) {
-      var total = 0;
-      var count = 0;
-      arr.forEach(function (item, index) {
-        total += item;
-        count++;
-      });
-      return total / count;
+  try {
+    const { room, senderId, review } = req.body;
+    if (!room || !senderId || !review) {
+      return res.status(404).send(`Invalid param`);
     }
-    const mean = calculateAverage(sum);
-    const result = await Room.findOneAndUpdate(room, {
-      review: mean,
-      total: total,
-    });
-    res.status(201).json({ result });
-  } else {
-    
-    await new Review({
-      room,
-      senderId,
-      review,
-    }).save();
-    const total = await Review.countDocuments({
-      room: room,
-    });
-    const roomReview = await Review.find({
-      room: room,
-    })
-    const sum = roomReview.map((a) => a.review);
 
-    function calculateAverage(arr) {
-      var total = 0;
-      var count = 0;
-      arr.forEach(function (item, index) {
-        total += item;
-        count++;
-      });
-      return total / count;
-    }
-    const mean = calculateAverage(sum);
-    const result = await Room.findByIdAndUpdate(room, {
-      review: mean,
-      total: total,
+    if (!mongoose.Types.ObjectId.isValid(room))
+      return res.status(404).send(`No room with id: ${room}`);
+
+    const reviewed = await Review.findOne({
+      room: room,
+      senderId: senderId,
     });
-    res.status(201).json({ result });
+    // WARN: REVIEWED
+    if (reviewed) {
+      await reviewed.updateOne({ room, senderId, review });
+      await reviewed.save();
+      const total = await Review.countDocuments({
+        room: room,
+      });
+      const roomReview = await Review.find({
+        room: room,
+      });
+      const sum = roomReview.map((a) => a.review);
+      function calculateAverage(arr) {
+        var total = 0;
+        var count = 0;
+        arr.forEach(function (item, index) {
+          total += item;
+          count++;
+        });
+        return total / count;
+      }
+      const mean = calculateAverage(sum);
+      await Room.findOneAndUpdate(room, {
+        review: mean,
+        total: total,
+      });
+      const result = await Room.findById(room)
+      console.log(result)
+      res.status(200).json(result);
+    } else {
+      await new Review({
+        room,
+        senderId,
+        review,
+      }).save();
+      const total = await Review.countDocuments({
+        room: room,
+      });
+      const roomReview = await Review.find({
+        room: room,
+      });
+      const sum = roomReview.map((a) => a.review);
+
+      function calculateAverage(arr) {
+        var total = 0;
+        var count = 0;
+        arr.forEach(function (item, index) {
+          total += item;
+          count++;
+        });
+        return total / count;
+      }
+      const mean = calculateAverage(sum);
+      await Room.findByIdAndUpdate(room, {
+        review: mean,
+        total: total,
+      });
+
+      const result = await Room.findById(room)
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
 };
 
