c8e3a6a10df4e9caa0394c99f81113267d148861
zan <olsencortuna@gmail.com>
2022-12-23 17:00:05 +0800

    feat: admin delete acc

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAcc, getAccs, subcribe, verifyAcc } from "../controllers/admin/acc.js";
+import { deleteAcc, getAcc, getAccs, subcribe, verifyAcc } from "../controllers/admin/acc.js";
 import { login } from "../controllers/admin/admin.js";
 import { getClients, verifyClient } from "../controllers/admin/client.js";
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/login", login);
 router.get("/getAccs", getAccs)
 router.get("/getAcc/:id", getAcc)
+router.delete("/deleteAcc/:id",deleteAcc)
 router.patch("/verifyAcc/:id", verifyAcc)
 router.get("/getClients", getClients)
 router.patch("/verifyClient/:id", verifyClient)
